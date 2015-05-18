fd2c9f84e71246a423ba582266e4ee2bc007fb11
long-short <long-short@myself.com>
2015-05-18 17:16:20 +0530

    Inserted the full code for makeCacheMatrix and cacheSolve
    As part of the programming assignment 2 in the R Programming Language
    Course on Coursera, made changes to the cloned repository, as required.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function Creates an inverted matrix and caches it
+## Second function checks for a cached inverted matrix and 
+##returns it if it exists or creates one and prints it
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## Creates an inverted matrix and caches it
+
+makeCacheMatrix <- function(x = matrix()) {  #for created a cached matrix
+  m_inv <- NULL  # sets value of m_inv to NULL (default value in case cacheSolve is yet to be used)
+  set <- function(y) {  #sets matrix value
+    x <<- y  #caches inputted matrix for cacheSolve to check if values have changed
+    m_inv <<- NULL  #sets value of m_inv (inversed matrix if used cacheSolve) to NULL
+  }
+  get <- function() x #gets data
+  setMat_Inv <- function(solve) m_inv <<- solve #caches inverted matrix
+  getMat_Inv <- function() m_inv #solves for inverted matrix 
+  list(set = set, get = get, # creates a list to store functions
+       setMat_Inv = setMat_Inv,
+       getMat_Inv = getMat_Inv)
 }
 
 
-## Write a short comment describing this function
+## Returns an inverted matrix, after checking if it has been cached earlier
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x, ...) { #for checking if a previous value has been cached
+  m_inv <- x$getMat_Inv()  #value to be used if an earlier Matrix has been inverted and input values have not changed
+  if(!is.null(m_inv)) { #checks if cacheSolve has run earlier and stores data
+    message("getting cached data")
+    return(m_inv) #provide inverted matrix
+  }
+  data <- x$get() #gets new values, if changed
+  m_inv <- solve(data, ...) #inverts matrix
+  x$setMat_Inv(m_inv) 
+  m_inv #prints inverted matrix
 }
